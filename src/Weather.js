2de601e6d477c2c74cbe038b43369583fbb34814
Aji Sainabou Njie <njieajisai@gmail.com>
2023-01-04 00:37:19 -0500

    Added the basic weather app structure and CSS design

--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
+
 export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`The weather in ${props.city} is ${response.data.main.temp}৹C`);
-  }
+  function handleResponse(response) {}
 
   let apiKey = "a89397bbee39ef0cd278072307619f0b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
+
   return (
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
+    <div className="container">
+      <div className="card1">
+        <form className="searchBar" id="searchForm">
+          <input
+            type="text"
+            placeholder="Enter city"
+            className="city-input"
+            id="search-city"
+            autofocus="on"
+            autocomplete="off"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="form-control btn btn-primary shadow-sm"
+            id="search-button"
+          />
+        </form>
+      </div>
+
+      <div className="card2" Name="temperature">
+        <div className="" Name="weatherToday">
+          <h2 id="city" className="" Name="city">
+            London
+          </h2>
+
+          <h5>
+            last updated at <span id="date">Tuesday 10:00</span>
+          </h5>
+
+          <ul id="units">
+            <strong>
+              <button href="#" id="celsius-temp" className="unit" Name="active">
+                {" "}
+                ºC{" "}
+              </button>
+              |
+              <button href="#" id="fahr-temp" className="unit" Name="">
+                {" "}
+                ºF{" "}
+              </button>
+            </strong>
+          </ul>
+
+          <h1 className="" Name="currentTemp" id="temp">
+            ☀️ 20
+          </h1>
+
+          <h3 id="description">#</h3>
+          <div className="" Name="row Others" id="others">
+            <div className="" Name="col-5 humidity">
+              Humidity
+              <br />
+              <strong>
+                <span className="" Name="humidity-value" id="humidity">
+                  48
+                </span>{" "}
+                %
+              </strong>
+            </div>
+            <div className="" Name="col-5 wind">
+              Wind
+              <br />
+              <strong>
+                <span className="" Name="wind-value" id="wind">
+                  3
+                </span>
+                <span className="" Name="windUnits" id="windUnits">
+                  m/s
+                </span>
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+      <small className="card3" Name="footer">
+        <a
+          href="https://github.com/Aji-Sainabou/vanilla"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source coded
+        </a>
+        by Aji sainabou Njie
+      </small>
+    </div>
   );
 }
